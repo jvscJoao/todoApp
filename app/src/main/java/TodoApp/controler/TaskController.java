586731408c922b2a381897a9e27e74eb77273bf4
jvscJoao <joaov1500@gmail.com>
2023-05-01 16:58:20 -0300

    Insert Controller

--- app/src/main/java/TodoApp/controler/TaskController.java
+++ app/src/main/java/TodoApp/controler/TaskController.java
@@ -3,6 +3,7 @@ package TodoApp.controler;
 
 import TodoApp.model.Task;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -11,7 +12,30 @@ import util.ConnectionFactory;
 
 public class TaskController {
     
-    public void insert(Task task) {
+    public void insert(Task task) throws SQLException {
+        
+        String sql = "INSERT INTO tasks (idProject, name, description, completed, notes, deadline, createdAt, updateAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        Connection cn = null;
+        PreparedStatement pt = null;
+        
+        try {
+            cn = ConnectionFactory.getConnection();
+            pt = cn.prepareStatement(sql);
+            pt.setInt(1, task.getId());
+            pt.setString(2, task.getName());
+            pt.setString(3, task.getDescription());
+            pt.setBoolean(4, task.isIsCompleted());
+            pt.setString(5, task.getNotes());
+            pt.setDate(6, new Date(task.getDeadline().getTime()));
+            pt.setDate(7, new Date(task.getCreatedAt().getTime()));
+            pt.setDate(8, new Date(task.getUpdatedAt().getTime()));
+            pt.execute();
+        } catch (Exception e) {
+            throw new RuntimeException("Error ao salvar a tarefa " + e.getMessage(), e);
+        } finally {
+            ConnectionFactory.closeConnection(cn);
+        }
         
     }
     
